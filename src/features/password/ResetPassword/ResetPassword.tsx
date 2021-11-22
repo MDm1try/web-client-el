33bdc7c889b8fae36305b1580b296e4d8a2fc7cd
Maria <maruskavadimovna@gmail.com>
2021-11-22 07:54:54 +0200

    Added isLoading mode while checking tocken

--- src/features/password/ResetPassword/ResetPassword.tsx
+++ src/features/password/ResetPassword/ResetPassword.tsx
@@ -1,18 +1,34 @@
 import usePasswordToken from "@/hooks/user/usePasswordToken";
 import useTranslation from "next-translate/useTranslation";
 
+import classcat from "classcat";
 import { ResetPasswordForm } from "./ResetPasswordForm";
+import css from "./ResetPassword.module.css";
 
 type Props = {
   token: string;
 };
 function ResetPassword({ token }: Props) {
   // todo add loading
-  const { data } = usePasswordToken(token);
+  const { isLoading, data } = usePasswordToken(token);
 
   const { t } = useTranslation(`errors`);
+
+  if (isLoading) {
+    return (
+      <div className={css.container}>
+        <div
+          className={classcat([css.spinnerWrapper, `spinner-border`])}
+          role="status"
+        >
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <>{data ? <ResetPasswordForm token={token} /> : t(`token-not-valid`)}</>
+    <> {data ? <ResetPasswordForm token={token} /> : t(`token-not-valid`)}</>
   );
 }
 
